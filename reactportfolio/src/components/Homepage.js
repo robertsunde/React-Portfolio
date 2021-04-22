2303637c643ef389ea64005698184dbc461c5eae
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-22 12:04:47 -0400

    beginning to route

---
+++ reactportfolio/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default class Homepage extends React.Component{
+
+ 
+//////////////////////////////////////////////////
+// RENDERS PAGE HEADER
+//////////////////////////////////////////////////
+
+render(){
+    return(
+    <><p className = "header" style={{textAlign:"center", fontSize:"36pt"}}>Homepage</p></>
+    
+    )}}
---
+++ reactportfolio/src/components/Porfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default class Portfolio extends React.Component{
+
+ 
+//////////////////////////////////////////////////
+// RENDERS PAGE HEADER
+//////////////////////////////////////////////////
+
+render(){
+    return(
+    <><p className = "header" style={{textAlign:"center", fontSize:"36pt"}}>Portfolio</p></>
+    
+    )}}
